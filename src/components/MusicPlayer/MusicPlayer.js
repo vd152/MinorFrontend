07a8825f37836bdf1f36aabe9c19b82448fa2339
Vidhi <vidhi152@gmail.com>
2021-09-27 19:11:11 +0530

    music player

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,11 +1,42 @@
 import React from "react";
-import './music.css'
+import "./music.css";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 class MusicPlayer extends React.Component {
-    render() {
-        return(
-            <div className="music-controls">Music player will go here!</div>
-        )
-    }
+  state = {
+    musicTracks: [
+      {
+        name: "Photo Album",
+        src: "https://www.bensound.com/bensound-music/bensound-photoalbum.mp3",
+      },
+      {
+        name: "November",
+        src: "https://www.bensound.com/bensound-music/bensound-november.mp3",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="music-controls">
+        <AudioPlayer
+          autoPlay={false}
+          // layout="horizontal"
+          src={this.state.musicTracks[0].src}
+          onPlay={(e) => console.log("onPlay")}
+          showSkipControls={true}
+          showJumpControls={false}
+          header={this.state.musicTracks[0].name}
+          // onClickPrevious={handleClickPrevious}
+          // onClickNext={handleClickNext}
+          // onEnded={handleClickNext}
+          listenInterval={5000}
+          onListen={(listen) => {
+            console.log(1);
+          }}
+        />
+      </div>
+    );
+  }
 }
 export default MusicPlayer;
